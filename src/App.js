b19f8dcb180b5ae63208b30363a8318649b70082
chae-chae <abed_nadir@naver.com>
2023-02-20 19:20:46 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("run all time");
+  const iRunOnlyOnce = () => {
+    console.log("run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
+    </div>
+  );
 }
 
 export default App;
