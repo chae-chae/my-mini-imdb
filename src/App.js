847def4942f2d091d9a3249cc6bb33ad830496b9
chae-chae <abed_nadir@naver.com>
2023-02-21 19:04:06 +0900

    map & filter practice with simple Todo app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("run all time");
-  const iRunOnlyOnce = () => {
-    console.log("run only once");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
   };
-  useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    console.log("SEARCH ", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("run when keyword|counter changes");
-  }, [keyword, counter]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDos((prev) => [toDo, ...prev]);
+    setToDo("");
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((_, todoIndex) => index !== todoIndex));
+  };
   return (
-    <div className="App">
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search.."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text"></input>
+        <button>add</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((i, index) => (
+          <li key={index}>
+            {i}
+            <button onClick={() => deleteBtn(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
